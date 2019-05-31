ac346303e64798ff90977448406e39f18256b8e8
claudie <leila.lord01@gmail.com>
2019-05-31 09:44:35 +0200

    Fist commit

---
+++ numeri.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//ho creato il conflitto
+int main() {
+	int contatore = 0;
+	for (int i = 1; i < 11; i++) {
+		contatore = i;
+		cout << contatore << endl;
+	}
+	<< << << < HEAD
+	cout << " Creare un conflitto " << endl;
+	cout << " Creare un conflitto " << endl;
+	== == == =
+	cout << " Ecco il conglitto " << endl;
+	>> >> >> > FIX_BUG
+		system("pause");
+}
